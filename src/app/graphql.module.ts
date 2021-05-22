3c8643721e758c9f6e0cbce869f72e100f51539c
7Mica <15919514+7Mica@users.noreply.github.com>
2021-05-22 00:10:46 -0600

    Added remove server for local enviorment.

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import { APOLLO_OPTIONS } from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 
-const uri = 'http://localhost:3000/graphql';
+const uri = 'http://dev.greenparrot.biz:3000/graphql';
 
 @NgModule({
   providers: [
