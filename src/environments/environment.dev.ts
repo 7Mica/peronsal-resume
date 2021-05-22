8725a35c06f624061e3ede45b8d334bd6c51dce9
7Mica <15919514+7Mica@users.noreply.github.com>
2021-05-22 09:29:03 -0600

    Added enverironment graphql uri server.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
+  production: false,
   graphQLServerUri: 'https://dev.greenparrot.biz:3000/graphql',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  graphQLServerUri: 'https://dev.greenparrot.biz:3000/graphql',
 };
 
 /*
