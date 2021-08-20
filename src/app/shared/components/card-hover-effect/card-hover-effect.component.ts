b867369ac4fe1898cfaaad4fbf1f63ed390dab0d
7Mica <15919514+7Mica@users.noreply.github.com>
2021-08-19 23:29:17 -0600

    PR-46: Retouch cards component.

--- src/app/shared/components/card-hover-effect/card-hover-effect.component.ts
+++ src/app/shared/components/card-hover-effect/card-hover-effect.component.ts
@@ -1,13 +1,3 @@
-import {
-  animate,
-  animateChild,
-  group,
-  query,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { IHobby } from '@core/interfaces/hobby.interface';
 
@@ -15,29 +5,8 @@ import { IHobby } from '@core/interfaces/hobby.interface';
   templateUrl: 'card-hover-effect.component.html',
   selector: 'card-hover-effect',
   styleUrls: ['card-hover-effect.component.scss'],
-  animations: [
-    trigger('animateHeight', [
-      state('init', style({ height: '*', transform: 'translateY(10%)' })),
-      state('ends', style({ height: '100%', transform: 'translateY(0)' })),
-
-      transition('* <=> *', [
-        group([query('@childAnimation', animateChild()), animate(400)]),
-      ]),
-    ]),
-    trigger('childAnimation', [
-      state('init', style({ transform: 'translateY(50%)', opacity: '0' })),
-      state('ends', style({ transform: 'translateY(0)', opacity: 1 })),
-      transition('* <=> *', animate(400)),
-    ]),
-  ],
 })
 export class CardHoverEffectComponent {
   @Input()
   hobby: IHobby | null = null;
-
-  public isTriggered = false;
-
-  get triggerEp(): string {
-    return this.isTriggered ? 'ends' : 'init';
-  }
 }
